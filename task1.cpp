c6f313a7505fe9e99da555e6262d30831c443485
f2025-0381 <f2025-0381@bnu.edu.pk>
2025-10-24 15:24:46 +0500

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "Muhammad essa sheikh";
+    string dept = "SCIT";
+    string quote = "\"jab ziada barish ata hai tou ziada pani ata hai.\" - bilawal bhutoo";
+
+    cout << "*******************************\n";
+    cout << "*\tName:\t\t" << name << "\n";
+    cout << "*\tDepartment:\t" << dept << "\n";
+    cout << "*\tQuote:\t\t" << quote << "\n";
+    cout << "*******************************\n";
